1d938e8449c72eefab4d6478b7ff1b913929872f
zhong.li@D1301004996 <zhong.li@united-imaging.com>
2022-11-18 10:40:21 +0800

    update

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,7 +3,7 @@
 const app = new App('https://c8826b5e495280843de15e1a126546dd.cn1.crs.easyar.com:8443');
 // 如果使用自定义方法获取token
 app.setToken({
-    'crsAppId': 'b5c729f7a1cfeb7911a0199523652d5b',
+    'crsAppId': '5ac417c4d19b5c839561419a47df0ab2',
     'token': 'nj6TPnm/SHnNSZZ/hRxgTuiv1boxZtrwZqIsStO4spOwuWAahbw9jB+NBFfYVPVYPU3r87JMXV4LYi7DLyRViVQASVg5zWfcRuZ7gWh3pf0V1FlT5qhsrrB0d69spi+VpyljBF/RI8aHIsEZzOxIOQgk2OvBnRnwxrmYuB6pSPKkGq63e62DE2s+20j5ig+XzT3TTRIQRijLmKNh6j6tbnip8ZApVhbRIISFIlNS/A/C5E5cqDjo4Czu4IsXKrkZqRwKnp+eEDgW5t4TnC4w3ENRHoRStgcSk+8iSuBkYqOxWGQLW/8sydvHDfFLbpALzWPRJCipT9PlejgA92VLJB3/U27YIqz38bvtTLTn2y05cNvD+ONXVUH/k6+f/5PobE9DViNI38J6y3Rw/bFDcpqKXpg7WAcJy0BLH/KL3MFLZymT1L85NdJwfdc38PKexCxZxf4Kipt8o4637jhwHu+0u+lqVaGEQhM8nNd+EsSp3IxKRh00jrEOhW/QoGLf37wU/+vb9ySwxU7hmevIynqLBu4kLIPIQt0PUaf9wLOQdI6pssLBWWCpbUmZMJblWaBfpkgckLMJVt7roZ6Ps6VGgoUeqyKQthtUa+Ifht3MZBGj/672mt0zutvoMrKy' // APIKey+APISecret生成的token
 });
 // 如果使用EasyAR提供的集成环境
